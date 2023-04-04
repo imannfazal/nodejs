4236ae145b819330e394a9e81e7fdf69ba8847b7
imannfazal <imannfazal@gmail.com>
2023-04-04 23:53:30 +0500

    pipe

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function (req, res){
+http.createServer(function (req, res) {
     const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
-    fileStream.on('open',()=>{
+    fileStream.on('open', () => {
         fileStream.pipe(res);
     })
 
-    fileStream.on('error',(err)=>{
+    fileStream.on('error', (err) => {
         res.end(err);
     })
 
     // const text= fs.readFileSync('./content/big.txt', 'utf-8')
     // res.end(text);
 })
-.listen(5000);
+    .listen(5000);
